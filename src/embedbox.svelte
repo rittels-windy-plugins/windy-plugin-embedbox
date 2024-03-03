5f09c8e86b904efebc877e318c03a6a5f9fa7bbd
chris <root@ChrisDell>
2024-03-03 10:55:43 +0200

    1st commit embedbox

---
+++ src/embedbox.svelte
@@ -0,0 +1,39 @@
+{@html embedHTML}
+
+<script>
+    //import bcast from '@windy/broadcast';
+    //import { onDestroy, onMount } from 'svelte';
+    import plugins from '@windy/plugins';
+
+    import config from './pluginConfig';
+
+    //import { globalCssNode, insertGlobalCss, removeGlobalCss } from './globalCss.js';
+
+    const { name } = config;
+
+   // let refs = {};
+
+    let embedHTML = '';
+
+    /*
+    export const onopen = _params => {
+    };
+
+    onMount(() => {
+        insertGlobalCss();
+    });
+
+    onDestroy(() => {
+        removeGlobalCss();
+    });
+
+    */
+    plugins[name].exports = {
+        html: str=>{ embedHTML = str }
+    };
+    
+</script>
+
+<style lang="less">
+    @import 'embedbox.less';
+</style>
